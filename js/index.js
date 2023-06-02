4669b0752c7524b5ab4851ea7a099c492b9d270e
jdussold <jdussold@gmail.com>
2023-06-02 12:08:45 -0500

    updated so that home page content would update dynamically depending on screen size

--- js/index.js
+++ js/index.js
@@ -22,4 +22,37 @@ window.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
     typeNextCharacter();
   }, 1000); // 1-second delay before starting the typing effect
+
+  // Code for dynamic text change
+  const codeBlock = document.querySelector(".code-block");
+  const comment1 = codeBlock.children[0];
+  const comment2 = document.createElement("span");
+  comment2.className = "comment";
+  comment2.textContent =
+    "// don't forget to play the game and see it on my Github";
+  const githubLink = codeBlock.querySelector("a");
+
+  function updateCodeBlockText() {
+    if (window.innerWidth <= 999) {
+      comment1.textContent = "// find my profile on Github:";
+      if (codeBlock.contains(comment2)) {
+        codeBlock.removeChild(comment2);
+      }
+      githubLink.href = "https://github.com/jdussold";
+      githubLink.textContent = "https://github.com/jdussold";
+    } else {
+      comment1.textContent = "// welcome to my portfolio";
+      if (!codeBlock.contains(comment2)) {
+        codeBlock.insertBefore(comment2, codeBlock.children[1]);
+      }
+      githubLink.href = "https://github.com/jdussold/space-invaders";
+      githubLink.textContent = "https://github.com/jdussold/space-invaders";
+    }
+  }
+
+  // Call the function initially to set correct text
+  updateCodeBlockText();
+
+  // Call the function whenever the window is resized
+  window.addEventListener("resize", updateCodeBlockText);
 });
