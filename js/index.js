ae9e965874368a53bd24c167f36b69caa799ba70
jdussold <jdussold@gmail.com>
2023-06-05 01:01:15 -0500

    updated text on index.htm.

--- js/index.js
+++ js/index.js
@@ -29,7 +29,7 @@ window.addEventListener("DOMContentLoaded", () => {
   const comment2 = document.createElement("span");
   comment2.className = "comment";
   comment2.textContent =
-    " don't forget to play the game and see it on my Github";
+    " Don't forget to play the game and see it on my Github";
   const githubLink = codeBlock.querySelector("a");
 
   function updateCodeBlockText() {
@@ -41,7 +41,7 @@ window.addEventListener("DOMContentLoaded", () => {
       githubLink.href = "https://github.com/jdussold";
       githubLink.textContent = "https://github.com/jdussold";
     } else {
-      comment1.textContent = "// welcome to my portfolio.";
+      comment1.textContent = "// Welcome to my portfolio.";
       if (!codeBlock.contains(comment2)) {
         codeBlock.insertBefore(comment2, codeBlock.children[1]);
       }
