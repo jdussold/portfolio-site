a5fa7ef03d7b75061b582cab2b27bc6dfaf7eff6
John Dussold <112672868+jdussold@users.noreply.github.com>
2026-05-10 22:22:07 -0500

    refactor: extract CSS design tokens, remove dead rules, move filter brightness to class

--- js/projects.js
+++ js/projects.js
@@ -35,16 +35,8 @@ function updateProjects() {
     }
   });
 
-  // Update the color and brightness of the list item
   checkboxes.forEach((checkbox) => {
-    const listItem = checkbox.closest("li");
-    if (checkbox.checked) {
-      listItem.style.color = "#FFFFFF";
-      listItem.style.filter = "brightness(150%)";
-    } else {
-      listItem.style.color = "";
-      listItem.style.filter = "";
-    }
+    checkbox.closest("li").classList.toggle("selected", checkbox.checked);
   });
 }
 
